cce749eb824ef456029be1a7054f093dce6cb25d
Сергей Пластовец <InKBiZiTOR2016@rambler.ru>
2022-02-21 14:07:39 +0300

    update 1.0: added path choose

--- LTD/Form1.Designer.cs
+++ LTD/Form1.Designer.cs
@@ -33,6 +33,11 @@ namespace LTD
             this.previous_image = new System.Windows.Forms.Button();
             this.opennewwindow = new System.Windows.Forms.Button();
             this.next_image = new System.Windows.Forms.Button();
+            this.exit_button = new System.Windows.Forms.Button();
+            this.list_of_files = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.find_directoty_path = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -41,7 +46,7 @@ namespace LTD
             this.pictureBox1.Image = global::LTD.Properties.Resources._0НГЮК_250_009_Данные_электрические_на_ас__двигатель_типа_АОД_630_4У1_;
             this.pictureBox1.Location = new System.Drawing.Point(46, 23);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(580, 690);
+            this.pictureBox1.Size = new System.Drawing.Size(658, 749);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -49,7 +54,7 @@ namespace LTD
             // previous_image
             // 
             this.previous_image.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.previous_image.Location = new System.Drawing.Point(46, 729);
+            this.previous_image.Location = new System.Drawing.Point(79, 796);
             this.previous_image.Name = "previous_image";
             this.previous_image.Size = new System.Drawing.Size(156, 68);
             this.previous_image.TabIndex = 1;
@@ -59,7 +64,7 @@ namespace LTD
             // opennewwindow
             // 
             this.opennewwindow.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.opennewwindow.Location = new System.Drawing.Point(257, 729);
+            this.opennewwindow.Location = new System.Drawing.Point(304, 796);
             this.opennewwindow.Name = "opennewwindow";
             this.opennewwindow.Size = new System.Drawing.Size(158, 68);
             this.opennewwindow.TabIndex = 2;
@@ -69,18 +74,74 @@ namespace LTD
             // next_image
             // 
             this.next_image.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.next_image.Location = new System.Drawing.Point(475, 729);
+            this.next_image.Location = new System.Drawing.Point(525, 796);
             this.next_image.Name = "next_image";
             this.next_image.Size = new System.Drawing.Size(151, 68);
             this.next_image.TabIndex = 3;
             this.next_image.Text = "Следующее изображение";
             this.next_image.UseVisualStyleBackColor = true;
             // 
+            // exit_button
+            // 
+            this.exit_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.exit_button.Location = new System.Drawing.Point(1183, 796);
+            this.exit_button.Name = "exit_button";
+            this.exit_button.Size = new System.Drawing.Size(140, 68);
+            this.exit_button.TabIndex = 4;
+            this.exit_button.Text = "Выход";
+            this.exit_button.UseVisualStyleBackColor = true;
+            // 
+            // list_of_files
+            // 
+            this.list_of_files.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.list_of_files.FormattingEnabled = true;
+            this.list_of_files.ItemHeight = 24;
+            this.list_of_files.Location = new System.Drawing.Point(735, 23);
+            this.list_of_files.Name = "list_of_files";
+            this.list_of_files.Size = new System.Drawing.Size(382, 196);
+            this.list_of_files.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(1126, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(216, 24);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Выберите директорию";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(1179, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 24);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "для работы";
+            // 
+            // find_directoty_path
+            // 
+            this.find_directoty_path.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.find_directoty_path.Location = new System.Drawing.Point(1166, 114);
+            this.find_directoty_path.Name = "find_directoty_path";
+            this.find_directoty_path.Size = new System.Drawing.Size(150, 37);
+            this.find_directoty_path.TabIndex = 8;
+            this.find_directoty_path.Text = "Обзор...";
+            this.find_directoty_path.UseVisualStyleBackColor = true;
+            this.find_directoty_path.Click += new System.EventHandler(this.find_directoty_path_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1219, 821);
+            this.ClientSize = new System.Drawing.Size(1354, 885);
+            this.Controls.Add(this.find_directoty_path);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.list_of_files);
+            this.Controls.Add(this.exit_button);
             this.Controls.Add(this.next_image);
             this.Controls.Add(this.opennewwindow);
             this.Controls.Add(this.previous_image);
@@ -90,6 +151,7 @@ namespace LTD
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -99,6 +161,11 @@ namespace LTD
         private System.Windows.Forms.Button previous_image;
         private System.Windows.Forms.Button opennewwindow;
         private System.Windows.Forms.Button next_image;
+        private System.Windows.Forms.Button exit_button;
+        private System.Windows.Forms.ListBox list_of_files;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button find_directoty_path;
     }
 }
 
--- LTD/Form1.cs
+++ LTD/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace LTD
 {
@@ -20,6 +21,21 @@ namespace LTD
         private void Form1_Load(object sender, EventArgs e)
         {
            // MessageBox.Show("HAHA");
+        }
+
+        private void find_directoty_path_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog FBD = new FolderBrowserDialog();
+            if (FBD.ShowDialog() == DialogResult.OK)
+            {
+                MessageBox.Show(FBD.SelectedPath);
+                DirectoryInfo dir = new DirectoryInfo(FBD.SelectedPath);
+                FileInfo[] files = dir.GetFiles("*.jpg");
+                foreach (FileInfo fi in files)
+                {
+                    list_of_files.Items.Add(fi.ToString());
+                }
+            }
         }
     }
 }
